8ff028bad46f24aefe6a2fe3f550e7a8f7e54adb
Alex Hanys <alexander.hanys1@marist.edu>
2022-01-20 21:00:09 -0500

    Full code and readme

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # DH-Sports-Auto-Email
+Note: I am working on making this an application that will require no code editing to run. This is how it is for now.
+## Setup
+This is only done once.
+### **Excel File Setup**
+
+1. In the Email list First remove the heading row and ID number column. 
+
+2. make sure that there are no blanks, and remove all rows that have blanks in them. 
+3. Save the file. Once saved, DO NOT MOVE THE FILE LOCATION. 
+
+### **Python Setup**
+
+1. go to https://www.python.org/downloads/ and download version 3.10.1. Make sure to install pip with it. you do not need the IDLE. Notepad will work fine for what we need to do, however downloading Visual Studio Code (https://code.visualstudio.com/) might make it easier to run the program. 
+
+2. Open Command Prompt and type the following Command:
+    - pip install openpyxl
+
+### **Code First Time Setup**
+
+1. On line 10 replace "Insert path to email list here" with the file path to the email list. Use \\\ (double backslash) between folders (i.e. users\\\documents). Keep in quotes. 
+
+2. On line 25 enter your email address and password in quotes.
+
+3. On line 94 enter your email address.
+
+### **Gmail Setup**
+
+1. On Google, click your User Icon and select "Manage your Google Account".
+
+2. Select "Security".
+
+3. Turn on "Less Secure App Access".
+
+## Setting Up the Script
+
+This is done each time you run it. 
+1. Download the ETA report workbook and open it.
+
+2. In file explorer, find the location of the file. 
+
+3. At the bar at the top, right click and select "Copy Address As Text". This will copy somthing like this to your clipboard: D:\abhmi\Documents
+
+4. Paste this in line 11, keeping the quotes.
+
+5. Add a second backslash after each one, and add another 2 at the end.
+
+6. Type **the exact name** of the excel workbook, with .xlsx at the end of it, after what was pasted in. 
+
+## Running the Script
+
+### If using VSC
+1. Click "Terminal" at the top, then "new terminal"
+
+2. Make sure you are using Command prompt and not Powershell.
+
+3. type "python emailer.py". This will run the program. DO NOT CLOSE ANYTHING UNTIL YOU SEE THE TIME ELAPSED. It will look like it's frozen for a while, but it is just loading the workbook.
+
+### If not using VSC
+
+1. Open Command prompt.
+
+2. use the "cd" and "dir" commands to navigate to the folder containing "emailer.py".
+
+3. type "python emailer.py". This will run the program. DO NOT CLOSE ANYTHING UNTIL YOU SEE THE TIME ELAPSED. It will look like it's frozen for a while, but it is just loading the workbook.
--- emailer.py
+++ emailer.py
@@ -2,90 +2,102 @@ from pickle import FALSE
 import openpyxl
 import smtplib
 from email.message import EmailMessage
+import time
 
-
+start = time.time()
 #file Locs
-emailList = "D:\\abhmi\\Documents\\test-email-book.xlsx"
-stockList = "D:\\abhmi\\Documents\\DH Open Sales Orders with ETAs January 11 2022 rerun TEST COPY.xlsx"
+#place destination of files here. Use \\ between folders.
+emailList = "Insert path to email list here"
+stockList = "insert path to stock list here"
 
 #open workbooks
 wbEmails = openpyxl.load_workbook(emailList)
 print("email list loaded")
 wbEmailsSheet = wbEmails.active
+print("Stock list loading, please do not close. it is not frozen.")
 wbStock = openpyxl.load_workbook(stockList)
 print("stock list loaded")
 
 #define area of emails list
-emailsCells = wbEmailsSheet['A1':'B1']
+emailsCells = wbEmailsSheet['A1':'B132']
 
-#printing values of email sheet
-for cell1, cell2, in emailsCells:
-    print(cell1.value, cell2.value)
-SelectedEmail = wbEmailsSheet["A1"].value
-
-#select stock sheet from wbemails
-wbStockSheet = wbStock.get_sheet_by_name(SelectedEmail)
-
-#define area of stock list
-stockCells = wbStockSheet['A1':'F250']
-
-#printing values of stock sheet
-message = ""
-skip = False
-for cell1, cell2, cell3, cell4, cell5, cell6 in stockCells:
-    if cell1.value == None and cell2.value == None and cell3.value == None and cell4.value == None and cell5.value == None and cell6.value == None and skip == True:
-        break
-    elif cell1.value == None and cell2.value == None and cell3.value == None and cell4.value == None and cell5.value == None and cell6.value == None and skip == False:
-        skip = True
-    
-    if (cell1.value != None):
-        one = cell1.value
-        one = str(one)
-        if one[0] in "0123456789":
-            one = (one[:10])
-        if isinstance(one, str):
-            one = one.ljust(19,".")
-    else:
-        one = "..................."
-    if (cell2.value != None):
-        two = cell2.value
-        two = two.ljust(7, ".")
-    else:
-        two = "......."
-    if (cell3.value != None):
-        three = cell3.value
-        three = three.ljust(11,".")
-    else:
-        three = "..........."
-    if (cell4.value != None):
-        four = cell4.value
-        four = four.ljust(46, ".")
-    else:
-        four = "."*46
-    if (cell5.value != None):
-        five = cell5.value
-        five = str(five)
-        five = five.ljust(14,".")
-    else:
-        five = "................"
-    if (cell6.value != None):
-        six = cell6.value
-        six = str(six)
-    else:
-        six = "     "
-    #print(one, two, three, four, five, six)
-    message = message + one +" "+ two +" "+ three +" "+ four +" "+ five +" "+ six +" "+ "\n" 
-    print (message)
-message = "<pre>" + message + "</pre>"
-msg = EmailMessage()
-msg.set_content(message)
-
-msg['Subject'] = 'please work'
-msg['From'] = "abhmind@gmail.com"
-msg['To'] = "abhmind@gmail.com"
-
-# Send the message via our own SMTP server.
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("abhmind@gmail.com", "fierland")
-server.send_message(msg)
+server.login("Insert your email", "Insert password")
+
+#loop everything in here?
+for row in emailsCells:
+    SelectedEmailName = wbEmailsSheet.cell(row=row[0].row, column=1).value
+    SelectedEmail = wbEmailsSheet.cell(row=row[0].row, column=2).value
+
+    #select stock sheet from wbemails
+    if SelectedEmailName in wbStock.sheetnames:
+        wbStockSheet = wbStock.get_sheet_by_name(SelectedEmailName)
+
+        #define area of stock list
+        stockCells = wbStockSheet['A1':'F250']
+
+        #printing values of stock sheet
+        message = ""
+        skip = False
+        for cell1, cell2, cell3, cell4, cell5, cell6 in stockCells:
+            if cell1.value == None and cell2.value == None and cell3.value == None and cell4.value == None and cell5.value == None and cell6.value == None and skip == True:
+                break
+            elif cell1.value == None and cell2.value == None and cell3.value == None and cell4.value == None and cell5.value == None and cell6.value == None and skip == False:
+                skip = True
+
+            if (cell1.value != None):
+                one = cell1.value
+                one = str(one)
+                if one[0] in "0123456789":
+                    one = (one[:10])
+                if isinstance(one, str):
+                    one = one.ljust(19,".")
+            else:
+                one = "..................."
+            if (cell2.value != None):
+                two = cell2.value
+                two = two.ljust(7, ".")
+                two = two + " |"
+            else:
+                two = "......."
+            if (cell3.value != None):
+                three = cell3.value
+                three = three.ljust(11,".")
+                three = three + " |"
+            else:
+                three = "..........."
+            if (cell4.value != None):
+                four = cell4.value
+                four = four.ljust(46, ".")
+            else:
+                four = "."*46
+            if (cell5.value != None):
+                five = cell5.value
+                five = str(five)
+                five = five.ljust(14,".")
+            else:
+                five = "................"
+            if (cell6.value != None):
+                six = cell6.value
+                six = str(six)
+            else:
+                six = "     "
+            #print(one, two, three, four, five, six)
+            message = message + "\n" + one +" "+ two +" "+ three +" "+ four +" "+ five +" "+ six +" "+ "\n" 
+        print (message)
+        message = message + "\n" + "This email was sent automatically. If there is an issue please reach out to davidhanys@gmail.com or alexanderhanys@gmail.com"
+        msg = EmailMessage()
+        msg.set_content(message)
+        
+        sendToEmail = wbEmailsSheet.cell(row=row[0].row, column=2).value
+        msg['Subject'] = "ETA Report - " + SelectedEmailName
+        msg['From'] = "Your Email here"
+        msg['To'] = SelectedEmail
+
+        # Send the message via SMTP server.
+        server.send_message(msg)
+    else:
+        print("No sheet found for " + SelectedEmailName)
 server.quit()
+end = time.time()
+print("Time taken: " + str(end - start))
